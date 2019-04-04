6a01650120f8a29f94735695e04ff1f79aff5009
Akash2021 <akashs.co.17@nsit,net.in>
2019-04-05 00:20:26 +0530

    Moved files into the folder and deleted the rest

--- Android-Projects/app/src/main/java/com/example/suraksha/activity_user.java
+++ Android-Projects/app/src/main/java/com/example/suraksha/activity_user.java
@@ -8,18 +8,27 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.material.navigation.NavigationView;
 
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.Locale;
+
 public class activity_user extends AppCompatActivity
 {
     private DrawerLayout drawerLayout;
     private ActionBarDrawerToggle actionBarDrawerToggle;
-    TextView usern;
+    private TextView usern;
+    private TextView resultTEXT;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -30,6 +39,50 @@ public class activity_user extends AppCompatActivity
         String name=bundle.getString("name");
         System.out.println("Name : "+name);
         usern.setText("WELCOME");
+
+        resultTEXT=findViewById(R.id.recordText);
+        resultTEXT.setVisibility(View.INVISIBLE);
+
+
+        Button navigate =findViewById(R.id.navigateButton);
+        navigate.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent=new Intent(activity_user.this,MapsDirection.class);
+                startActivity(intent);
+            }
+        });
+
+
+        //////////////////////////BUTTON FOR ANALYSE//////////////////////////
+
+        Button analyse=findViewById(R.id.analyseButton);
+        analyse.setOnClickListener(new View.OnClickListener()
+        {
+
+            @Override
+            public void onClick(View v)
+            {
+                resultTEXT.setVisibility(View.VISIBLE);
+                System.out.println(resultTEXT.getText().toString());
+                if(resultTEXT.getText().toString()=="help")
+                {
+                    Intent callIntent = new Intent(Intent.ACTION_CALL);
+                    callIntent.setData(Uri.parse("tel:100"));
+                    startActivity(callIntent);
+                    Toast.makeText(activity_user.this, "Emergency", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
+
+        //////////////////////////BUTTON FOR ANALYSE//////////////////////////
+
+
+
 
         drawerLayout=findViewById(R.id.drawerLayout);
         actionBarDrawerToggle=new ActionBarDrawerToggle(this,drawerLayout,R.string.Open,R.string.Close);
@@ -74,6 +127,8 @@ public class activity_user extends AppCompatActivity
                 else if(id==R.id.selfDefense)
                 {
                     ///IMPLEMENT ON CLICK LISTENER
+                    Intent intent=new Intent(activity_user.this,SelfDefense.class);
+                    startActivity(intent);
                     Toast.makeText(activity_user.this, "Self Defence", Toast.LENGTH_SHORT).show();
                 }
 
@@ -81,6 +136,46 @@ public class activity_user extends AppCompatActivity
             }
         });
 
+    }
+
+    public void onClickRecord(View view)
+    {
+        if(view.getId()==R.id.recordButton)
+        {
+            promptSpeechInput();
+        }
+    }
+
+    public void promptSpeechInput()
+    {
+        Intent intent=new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT,"Say Something!");
+        try{
+            startActivityForResult(intent,100);
+        }
+        catch (Exception e)
+        {
+            Toast.makeText(activity_user.this,"Sorry! Your device does not support speech language", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void onActivityResult(int request_code,int result_code,Intent intent)
+    {
+        super.onActivityResult(request_code,result_code,intent);
+
+        switch (request_code)
+        {
+            case 100:
+                if(result_code==RESULT_OK&& intent!=null)
+                {
+                    ArrayList<String> result=intent.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    resultTEXT.setText(result.get(0));
+
+                }
+                break;
+        }
     }
 
 
